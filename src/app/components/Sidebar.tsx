a5aa0a257b0c232a6144eaa16bd1d9f677342996
Anshul Bhaduria <anshulsingh80@gmail.com>
2025-01-17 18:38:54 +0530

    update the sidebar functionality

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
-import { useEffect } from 'react'
-// import $ from 'jquery'
+import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/bootstrap.min.css'
 import '../styles/style.css'
@@ -14,34 +13,28 @@ import '../styles/all.min.css'
 import '../styles/bootstrap-datetimepicker.min.css'
 import '../styles/owl.carousel.min.css'
 import '../styles/owl.theme.default.min.css'
-import '../../../public/js/slimscroll.js'
-// import dynamic from 'next/dynamic'
-
-// const SlimScrollExample = dynamic(() => import('./SlimScrollExample'), {
-//   ssr: false
-// })
 
 const Sidebar = () => {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min.js')
-  }, [])
+  })
 
-  // useEffect(() => {
-  //   // Ensure SlimScroll is applied only on the client
-  //   if (typeof window !== 'undefined') {
-  //     $('#scrollable-container').slimScroll({
-  //       height: '300px',
-  //       color: '#ff6600',
-  //       size: '10px',
-  //       alwaysVisible: true
-  //     })
-  //   }
-  // }, [])
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <div className='main-wrapper'>
       <div className='sidebar' id='sidebar'>
-        <div className='sidebar-inner slimscroll'>
+        <div
+          className='sidebar-inner slimscroll overflow-auto'
+          style={{
+            border: '1px solid #ddd',
+            padding: '10px'
+          }}
+        >
           <div id='sidebar-menu' className='sidebar-menu'>
             <ul>
               <li>
@@ -89,13 +82,13 @@ const Sidebar = () => {
                       </li>
                     </ul>
                   </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
+                  <li className={`submenu ${isOpen ? 'open' : ''}`}>
+                    <a href='#' onClick={toggleMenu}>
                       <i className='ti ti-layout-list'></i>
                       <span>Application</span>
                       <span className='menu-arrow'></span>
                     </a>
-                    <ul>
+                    <ul style={{ display: isOpen ? 'block' : 'none' }}>
                       <li>
                         <a href='chat.html'>Chat</a>
                       </li>
@@ -1042,362 +1035,6 @@ const Sidebar = () => {
                       </li>
                       <li>
                         <a href='ban-ip-address.html'>Ban IP Address</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-
-              <li>
-                <h6 className='submenu-hdr'>
-                  <span>UI Interface</span>
-                </h6>
-                <ul>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-hierarchy-2'></i>
-                      <span>Base UI</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='ui-alerts.html'>Alerts</a>
-                      </li>
-                      <li>
-                        <a href='ui-accordion.html'>Accordion</a>
-                      </li>
-                      <li>
-                        <a href='ui-avatar.html'>Avatar</a>
-                      </li>
-                      <li>
-                        <a href='ui-badges.html'>Badges</a>
-                      </li>
-                      <li>
-                        <a href='ui-borders.html'>Border</a>
-                      </li>
-                      <li>
-                        <a href='ui-buttons.html'>Buttons</a>
-                      </li>
-                      <li>
-                        <a href='ui-buttons-group.html'>Button Group</a>
-                      </li>
-                      <li>
-                        <a href='ui-breadcrumb.html'>Breadcrumb</a>
-                      </li>
-                      <li>
-                        <a href='ui-cards.html'>Card</a>
-                      </li>
-                      <li>
-                        <a href='ui-carousel.html'>Carousel</a>
-                      </li>
-                      <li>
-                        <a href='ui-colors.html'>Colors</a>
-                      </li>
-                      <li>
-                        <a href='ui-dropdowns.html'>Dropdowns</a>
-                      </li>
-                      <li>
-                        <a href='ui-grid.html'>Grid</a>
-                      </li>
-                      <li>
-                        <a href='ui-images.html'>Images</a>
-                      </li>
-                      <li>
-                        <a href='ui-lightbox.html'>Lightbox</a>
-                      </li>
-                      <li>
-                        <a href='ui-media.html'>Media</a>
-                      </li>
-                      <li>
-                        <a href='ui-modals.html'>Modals</a>
-                      </li>
-                      <li>
-                        <a href='ui-offcanvas.html'>Offcanvas</a>
-                      </li>
-                      <li>
-                        <a href='ui-pagination.html'>Pagination</a>
-                      </li>
-                      <li>
-                        <a href='ui-popovers.html'>Popovers</a>
-                      </li>
-                      <li>
-                        <a href='ui-progress.html'>Progress</a>
-                      </li>
-                      <li>
-                        <a href='ui-placeholders.html'>Placeholders</a>
-                      </li>
-                      <li>
-                        <a href='ui-spinner.html'>Spinner</a>
-                      </li>
-                      <li>
-                        <a href='ui-sweetalerts.html'>Sweet Alerts</a>
-                      </li>
-                      <li>
-                        <a href='ui-nav-tabs.html'>Tabs</a>
-                      </li>
-                      <li>
-                        <a href='ui-toasts.html'>Toasts</a>
-                      </li>
-                      <li>
-                        <a href='ui-tooltips.html'>Tooltips</a>
-                      </li>
-                      <li>
-                        <a href='ui-typography.html'>Typography</a>
-                      </li>
-                      <li>
-                        <a href='ui-video.html'>Video</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-hierarchy-3'></i>
-                      <span>Advanced UI</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='ui-ribbon.html'>Ribbon</a>
-                      </li>
-                      <li>
-                        <a href='ui-clipboard.html'>Clipboard</a>
-                      </li>
-                      <li>
-                        <a href='ui-drag-drop.html'>Drag & Drop</a>
-                      </li>
-                      <li>
-                        <a href='ui-rangeslider.html'>Range Slider</a>
-                      </li>
-                      <li>
-                        <a href='ui-rating.html'>Rating</a>
-                      </li>
-                      <li>
-                        <a href='ui-text-editor.html'>Text Editor</a>
-                      </li>
-                      <li>
-                        <a href='ui-counter.html'>Counter</a>
-                      </li>
-                      <li>
-                        <a href='ui-scrollbar.html'>Scrollbar</a>
-                      </li>
-                      <li>
-                        <a href='ui-stickynote.html'>Sticky Note</a>
-                      </li>
-                      <li>
-                        <a href='ui-timeline.html'>Timeline</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-chart-line'></i> <span>Charts</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='chart-apex.html'>Apex Charts</a>
-                      </li>
-                      <li>
-                        <a href='chart-c3.html'>Chart C3</a>
-                      </li>
-                      <li>
-                        <a href='chart-js.html'>Chart Js</a>
-                      </li>
-                      <li>
-                        <a href='chart-morris.html'>Morris Charts</a>
-                      </li>
-                      <li>
-                        <a href='chart-flot.html'>Flot Charts</a>
-                      </li>
-                      <li>
-                        <a href='chart-peity.html'>Peity Charts</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-icons'></i> <span>Icons</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='icon-fontawesome.html'>Fontawesome Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-feather.html'>Feather Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-ionic.html'>Ionic Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-material.html'>Material Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-pe7.html'>Pe7 Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-simpleline.html'>Simpleline Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-themify.html'>Themify Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-weather.html'>Weather Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-typicon.html'>Typicon Icons</a>
-                      </li>
-                      <li>
-                        <a href='icon-flag.html'>Flag Icons</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-input-search'></i>
-                      <span>Forms</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li className='submenu submenu-two'>
-                        <a href='javascript:void(0);'>
-                          Form Elements
-                          <span className='menu-arrow inside-submenu'></span>
-                        </a>
-                        <ul>
-                          <li>
-                            <a href='form-basic-inputs.html'>Basic Inputs</a>
-                          </li>
-                          <li>
-                            <a href='form-checkbox-radios.html'>
-                              Checkbox & Radios
-                            </a>
-                          </li>
-                          <li>
-                            <a href='form-input-groups.html'>Input Groups</a>
-                          </li>
-                          <li>
-                            <a href='form-grid-gutters.html'>Grid & Gutters</a>
-                          </li>
-                          <li>
-                            <a href='form-select.html'>Form Select</a>
-                          </li>
-                          <li>
-                            <a href='form-mask.html'>Input Masks</a>
-                          </li>
-                          <li>
-                            <a href='form-fileupload.html'>File Uploads</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className='submenu submenu-two'>
-                        <a href='javascript:void(0);'>
-                          Layouts
-                          <span className='menu-arrow inside-submenu'></span>
-                        </a>
-                        <ul>
-                          <li>
-                            <a href='form-horizontal.html'>Horizontal Form</a>
-                          </li>
-                          <li>
-                            <a href='form-vertical.html'>Vertical Form</a>
-                          </li>
-                          <li>
-                            <a href='form-floating-labels.html'>
-                              Floating Labels
-                            </a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href='form-validation.html'>Form Validation</a>
-                      </li>
-                      <li>
-                        <a href='form-select2.html'>Select2</a>
-                      </li>
-                      <li>
-                        <a href='form-wizard.html'>Form Wizard</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-table-plus'></i>
-                      <span>Tables</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='tables-basic.html'>Basic Tables </a>
-                      </li>
-                      <li>
-                        <a href='data-tables.html'>Data Table </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h6 className='submenu-hdr'>
-                  <span>Help</span>
-                </h6>
-                <ul>
-                  <li>
-                    <a href='https://preschool.dreamstechnologies.com/documentation/index.html'>
-                      <i className='ti ti-file-text'></i>
-                      <span>Documentation</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href='https://preschool.dreamstechnologies.com/documentation/changelog.html'>
-                      <i className='ti ti-exchange'></i>
-                      <span>Changelog</span>
-                      <span className='badge badge-primary badge-xs fs-10 ms-auto text-white'>
-                        v1.8.3
-                      </span>
-                    </a>
-                  </li>
-                  <li className='submenu'>
-                    <a href='javascript:void(0);'>
-                      <i className='ti ti-menu-2'></i>
-                      <span>Multi Level</span>
-                      <span className='menu-arrow'></span>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href='javascript:void(0);'>Multilevel 1</a>
-                      </li>
-                      <li className='submenu submenu-two'>
-                        <a href='javascript:void(0);'>
-                          Multilevel 2
-                          <span className='menu-arrow inside-submenu'></span>
-                        </a>
-                        <ul>
-                          <li>
-                            <a href='javascript:void(0);'>Multilevel 2.1</a>
-                          </li>
-                          <li className='submenu submenu-two submenu-three'>
-                            <a href='javascript:void(0);'>
-                              Multilevel 2.2
-                              <span className='menu-arrow inside-submenu inside-submenu-two'></span>
-                            </a>
-                            <ul>
-                              <li>
-                                <a href='javascript:void(0);'>
-                                  Multilevel 2.2.1
-                                </a>
-                              </li>
-                              <li>
-                                <a href='javascript:void(0);'>
-                                  Multilevel 2.2.2
-                                </a>
-                              </li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href='javascript:void(0);'>Multilevel 3</a>
                       </li>
                     </ul>
                   </li>
